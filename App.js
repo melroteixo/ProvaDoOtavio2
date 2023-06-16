714c4757a173ddc9f36c026aa1b91325ef18412f
melroteixo <101290913+melroteixo@users.noreply.github.com>
2023-06-16 10:17:51 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import CadastrarUsuario from './src/pages/cadastro/cadastrarUsuario';
+import Catalogo from './src/pages/catalogo/menuItens';
+import Dashboard from './src/pages/dashBoard/usuarioDashboard';
+import Login from './src/pages/login/loginPage';
+import RedefinirSenha from './src/pages/recuperarSenha/recuperacaodeSenhas';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer style={styles.fundo}>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="MudarSenha" component={RedefinirSenha}/>
+        <Stack.Screen name="Registro" component={CadastrarUsuario}/>
+        <Stack.Screen name="Catalogo" component={Catalogo}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  fundo: {
+    backgroundColor: '#fff',
+  },
+});
